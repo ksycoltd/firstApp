de0038e8b3fe430c82cb93a857b11caa11dc0252
김 상연(Kim SangYeon) <sekim@nuskin.com>
2016-05-04 16:11:26 +0900

    Initial Commit

---
+++ How Many Fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by nuskin on 2016. 5. 4..
+//  Copyright © 2016년 nuskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
